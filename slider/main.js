032da2f2886e9ec5a189c798a3e137495f0e7201
Xpamoff <you@example.com>
2021-09-05 21:30:21 +0400

    123

--- slider/main.js
+++ slider/main.js
@@ -1,32 +1,18 @@
 let r1 = document.getElementById("r1");
 let r2 = document.getElementById("r2");
 let r3 = document.getElementById("r3");
-let r4 = document.getElementById("r4");
 
 let count = 1;
-
-let slideInter = setInterval(() => {
-    count++;
-    console.log(count);
-    if(count == 2){
+setInterval(()=>{
+    if(count%3==0){
+        r1.checked = true;
+    }
+    else if(count%3==1){
         r2.checked = true;
     }
-    else{
-        if(count == 3){
-            r3.checked = true;
-        }
-        else {
-            if(count == 4){
-            r4.checked = true;
-            }
-            else{
-                r1.checked = true;
-                count = 1;
-            }
-        }
+    else if(count%3==2){
+        r3.checked = true;
     }
+    count++;
 }, 3000);
 
-function onLabel(){
-    clearInterval(slideInter);
-}
